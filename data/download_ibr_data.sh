2fd2665eaebd8a03eb065b4e5b16d7519391d0f8
Yichen Sheng <sheng30@purdue.edu>
2023-11-10 13:31:00 -0500

    add eval download script

--- data/download_ibr_data.sh
+++ data/download_ibr_data.sh
@@ -1,4 +1,12 @@
-./dbxcli put Dataset/ibrnet/data/real_iconic_ibrnet.zip real_iconic_ibrnet.zip
-./dbxcli put Dataset/ibrnet/data/space.zip space.zip 
-./dbxcli put Dataset/ibrnet/data/realestate_nerf_synthetic.zip realestate_nerf_synthetic.zip 
-./dbxcli put Dataset/ibrnet/data/deepvoxels_nerf_llff_data.zip deepvoxels_nerf_llff_data.zip
+./dbxcli get Dataset/ibrnet/data/nerf_synthetic.zip nerf_synthetic.zip
+unzip nerf_synthetic.zip
+
+./dbxcli get Dataset/ibrnet/data/space.zip space.zip 
+unzip space.zip
+
+
+./dbxcli get Dataset/ibrnet/data/realestate.zip realestate.zip 
+unzip realestate.zip
+
+./dbxcli get Dataset/ibrnet/data/deepvoxels_nerf_llff_data.zip deepvoxels_nerf_llff_data.zip
+unzip deepvoxels_nerf_llff_data.zip
