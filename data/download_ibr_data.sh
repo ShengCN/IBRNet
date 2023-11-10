8a44a156c7ccaca9918488a337f62d1480522cfa
Yichen Sheng <sheng30@purdue.edu>
2023-11-09 23:15:24 -0500

    add download upload scripts

---
+++ data/download_ibr_data.sh
@@ -0,0 +1,4 @@
+./dbxcli put Dataset/ibrnet/data/real_iconic_ibrnet.zip real_iconic_ibrnet.zip
+./dbxcli put Dataset/ibrnet/data/space.zip space.zip 
+./dbxcli put Dataset/ibrnet/data/realestate_nerf_synthetic.zip realestate_nerf_synthetic.zip 
+./dbxcli put Dataset/ibrnet/data/deepvoxels_nerf_llff_data.zip deepvoxels_nerf_llff_data.zip
---
+++ data/upoad_ibr_data.sh
@@ -0,0 +1,4 @@
+./dbxcli put real_iconic_ibrnet.zip Dataset/ibrnet/data/real_iconic_ibrnet.zip
+./dbxcli put space.zip Dataset/ibrnet/data/space.zip
+./dbxcli put realestate_nerf_synthetic.zip Dataset/ibrnet/data/realestate_nerf_synthetic.zip
+./dbxcli put deepvoxels_nerf_llff_data.zip Dataset/ibrnet/data/deepvoxels_nerf_llff_data.zip
